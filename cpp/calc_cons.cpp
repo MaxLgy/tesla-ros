79da191382e8d6e855d649bbd04fa651872eb884
Nicoodo <nicolas.odorico@ensta-bretagne.org>
2021-04-09 09:45:58 +0200

    Mise à jour nodes consigne et control

--- cpp/calc_cons.cpp
+++ cpp/calc_cons.cpp
@@ -1,23 +1,70 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "visualization_msgs/Marker.h"
+#include <cmath>
+#include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Pose2D.h"
+#include "tf/tf.h"
+#include <cstdlib>
+#include "std_msgs/Float64.h"
 
+float theta_ligne = 0;
+float angle_roue = 0;
+float speed_motor = 0;
+float speed_motor_max = 2000;
+float k = 2;
+void getposligne(const std_msgs::Float64::ConstPtr& msg)
+{
+	theta_ligne = msg->data;
+}
+
+void getwheelangle(const std_msgs::Float64::ConstPtr& msg)
+{
+   angle_roue = msg->data;
+}
+
+void getmotorspeed(const std_msgs::Float64::ConstPtr& msg) 
+{
+	speed_motor = msg->data;
+}
+
+float consangle()
+{
+	float thetad;
+	thetad = theta_ligne;
+	return thetad;
+
+
+}
+
+float consspeed()
+{
+	float speed_motor;
+
+	speed_motor = speed_motor_max - k * theta_ligne;
+	return speed_motor;
+}
 int main(int argc, char **argv)
 {
-	ros::init(argc,argv,"controleur");
+	ros::init(argc,argv,"consigne");
 	ros::NodeHandle n;
 
     // Subscriber: 
-	ros::Subscriber pos_ligne_sub   = n.subscribe("position_ligne",1000,recupconsigne);
-    ros::Subscriber angl_roues_sub  = n.subscribe("angle_roues",1000,recupconsigne);
-    ros::Subscriber vitesse_mot_sub = n.subscribe("vitesse_moteur",1000,recupconsigne);
+	ros::Subscriber pos_ligne_sub   = n.subscribe("position_ligne",1000,getposligne);
+    ros::Subscriber angl_roues_sub  = n.subscribe("angle_roues",1000,getwheelangle);
+    ros::Subscriber vitesse_mot_sub = n.subscribe("vitesse_moteur",1000,getmotorspeed);
 
     // Publisher: 
-	ros::Publisher cons_pub = n.advertise<std_msgs::Float64>("consigne",1000);
+	ros::Publisher cons_pub = n.advertise<geometry_msgs::Pose2D>("consigne",1000);
 
 	ros::Rate loop_rate(25);
 
 	while(ros::ok())
 	{	
+		geometry_msgs::Pose2D msg;
+		msg.x = consspeed();
+		std::cout<<"send speed "<<msg.x<<std::endl;
+		msg.y = consangle();
+		cons_pub.publish(msg);
         ros::spinOnce();
 		loop_rate.sleep();
     }
--- cpp/contrôleur.cpp
+++ cpp/contrôleur.cpp
@@ -2,40 +2,43 @@
 #include "visualization_msgs/Marker.h"
 #include <cmath>
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Pose2D.h"
 #include "tf/tf.h"
 #include <cstdlib>
 #include "std_msgs/Float64.h"
-float cons1;
-float cons2;
+float cons1; //vitesse moteur voulue
+float cons2; //angle roue voulue 
 float cons3;
 
-void recupconsigne(const geometry_msgs::Pose2D::ConstPtr& msg)
+void cons(const geometry_msgs::Pose2D::ConstPtr& msg)
 {
-	cons1 = msg->Pose.x;
-	cons2 = msg->Pose.y;
-	cons3 = msg->Pose.theta;
+	cons1 = msg->x;
+	cons2 = msg->y;
+	cons3 = msg->theta;
 }
 
 float control_servo()
-{  float control
+{  float control;
 
-	return control
+	return control;
 }
 float control_brush()
-{  float control
+{  float control;
 
-	return control
+	return control;
 }
 int main(int argc, char **argv)
 {
 	ros::init(argc,argv,"controleur");
 	ros::NodeHandle n;
-	ros::Subscriber subcons = n.subscribe("consigne",1000,recupconsigne);
+	ros::Subscriber subcons = n.subscribe("consigne",1000,cons);
 	ros::Publisher brush_pub = n.advertise<std_msgs::Float64>("commande_direction",1000);
 	ros::Publisher servo_pub = n.advertise<std_msgs::Float64>("commande_vitesse",1000);
 	ros::Rate loop_rate(25);
 	while(ros::ok())
-	{	std_msgs::Float64 msgbrush;
+	{	
+		std::cout<<"received speed "<<cons1<<std::endl;
+		std_msgs::Float64 msgbrush;
 		std_msgs::Float64 msgservo;
 		msgservo.data = control_servo();
 		msgbrush.data = control_brush();
